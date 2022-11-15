c4a72bfff9d38c1ee51b14fbc4f2b4e0c9c22ac2
Morinbalo <morinbalogun@gmail.com>
2022-11-15 18:36:26 +0000

    version-one commit

--- script.js
+++ script.js
@@ -15,32 +15,19 @@ stopst.addEventListener('click',stoptime)
 
 function startbn() {
     countTens++
-    if (countTens<10){
-        tens.innerHTML='0'+countTens
 
-    }
-    else {tens.innerHTML=countTens}
-    
+    tens.innerHTML=countTens
+
 if  (countTens ===99){
     countTens=00
     countSec++
     sec.innerHTML=countSec
 
-   if (countSec<10){
-    sec.innerHTML='0'+countSec
-
-   } 
-   else{   sec.innerHTML=countSec }
     if (countSec===60) {
         countSec=00
   countHundred++
   hundred.innerHTML=countHundred
-  if (countHundred<10){
-    hundred.innerHTML='0'+countHundred
 
-  }
-  else {hundred.innerHTML=countHundred
-  }
 countTens++
 
 }
@@ -52,27 +39,23 @@ countTens++
 
 function inter (){
 intervals=setInterval(startbn,10)
-    
-    
 
-    
+
+
+
 }
 function stoptime() {
    clearInterval(intervals) 
-  
+
 
 }
 function resetst() {
     clearInterval(intervals)
-    countTens=00
-    countHundred=00
-    countSec=00
-    
-    hundred.innerHTML='0'+countHundred
-    sec.innerHTML ='0'+countSec
-    tens.innerHTML='0'+countTens
-   
-    
-    
-    
+    hundred.innerHTML=0
+    sec.innerHTML=0
+    tens.innerHTML=0
+
+
+
 }
+ 
